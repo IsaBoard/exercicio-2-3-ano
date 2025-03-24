3393386203fb7cd52cba509e94468a5a4004c5fb
IsaBoard <isabela.6933@aluno.pr.senac.br>
2025-03-24 12:10:06 -0300

    mensagem

--- gruntfile.js
+++ gruntfile.js
@@ -1,27 +1,5 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        sass: {
-            dist:{
-                options:{
-                    style: 'compressed'
-                },
-                files:{
-                    'dist/styles/main.css': 'src/styles/main.scss'
-                }
-            }
-        },
-        watch:{
-            sass:{
-                files:'scr/styles/**/*.scss',
-                tasks:['sass']
-            }
-        }
-        
+        pkg: grunt.file.readJson('packge.json'),
     })
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-
-    grunt.registerTask('default', ['sass', 'watch']);
-    
 }
